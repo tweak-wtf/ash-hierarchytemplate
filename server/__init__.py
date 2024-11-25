92a0ef835a934b89ddff803d345a589524433dee
tweak <me@tweak.wtf>
2024-11-25 21:47:08 +0100

    use package.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,4 @@
 from typing import Type
-from .version import __version__
 from .settings import HierarchyTemplateAddonSettings, DEFAULT_VALUES
 
 from ayon_server.addons import BaseServerAddon
@@ -7,21 +6,11 @@ from ayon_server.addons import BaseServerAddon
 
 
 class HierarchyTemplateAddon(BaseServerAddon):
-    name = "HierarchyTemplate"
-    title = "Hierarchy Template"
-    version = __version__
     settings_model: Type[
         HierarchyTemplateAddonSettings
     ] = HierarchyTemplateAddonSettings
 
     # frontend_scopes: dict[str, Any] = {"settings": {}}
-
-    # this variable actually exposes theese services to the webui
-    services = {
-        "HierarchyTemplateProcessor": {
-            "image": f"doerp/ash-hierarchytemplate-processor:{version}",
-        },
-    }
 
     async def get_default_settings(self):
         settings_model_cls = self.get_settings_model()
