f63ace6e3a757a081721b365e9e9ebeda8f06f5b
tweak <me@tweak.wtf>
2024-11-26 20:05:02 +0100

    implements launcher action compatibility

--- client/ash_hierarchytemplate/plugins/actions/apply_hierarchy_template.py
+++ client/ash_hierarchytemplate/plugins/actions/apply_hierarchy_template.py
@@ -4,18 +4,24 @@ from ayon_core.pipeline import LauncherAction
 
 class ApplyHierarchyTemplate(LauncherAction):
     """Open AYON browser page to the current context."""
+
     name = "apply_hierarchy_template"
     label = "Apply Hierarchy Template"
     icon = "gears"
     order = 999
 
-    # TODO: add filter where this is accessible from
+    def is_compatible(self, selection):
+        self.log.info(f"selection: {selection}")
+        folder_path = selection.get_folder_path()
+        if folder_path == "/":
+            return True
+        return False
 
     def process(self, selection, **kwargs):
-      ayon_api.dispatch_event(
-         topic="apply_hierarchy_template",
-         sender=self,
-         project_name=selection["project_name"],
-         username=selection["username"],
-      )
-      # open events page with filter?
+        ayon_api.dispatch_event(
+            topic="apply_hierarchy_template",
+            sender=self,
+            project_name=selection["project_name"],
+            username=selection["username"],
+        )
+        # open events page with filter?
