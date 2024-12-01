f81e7d33c48ee204566e8fbd9c550fd098d32f3f
doerp <me@doerp.cool>
2024-12-01 15:19:50 +0100

    strips prerelease tag

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "hierarchytemplate"
 title = "HierarchyTemplate"
-version = "0.2.1-dev"
+version = "0.2.1"
 
 services = {
     "HierarchyTemplateProcessor": {"image": f"doerp/ash-hierarchytemplate-processor:{version}"},
