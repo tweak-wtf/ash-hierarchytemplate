be441efe09d30766322c57ab238b042c24bc550f
doerp <me@doerp.cool>
2025-05-23 19:19:56 +0200

    bumps dev version

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "hierarchytemplate"
 title = "HierarchyTemplate"
-version = "0.2.3"
+version = "0.2.4-dev"
 
 services = {
     "HierarchyTemplateProcessor": {"image": f"doerp/ash-hierarchytemplate-processor:{version}"},
